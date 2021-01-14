a5845ceb84cbe3474883d10656af4bf9a1936dfe
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2021-01-14 12:37:18 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,6 +4,89 @@
 #include <string>
 #include <type_traits>
 
+#include <iostream>
+
 // tutaj klasa Penne
 
+class Makaron
+{
+public:
+    Makaron() = default;
+    ~Makaron() = default;
+
+public:
+    virtual double ileMaki(unsigned) const = 0;
+
+    static Makaron* gotujMakaron(std::string&);
+};
+class Penne : public Makaron
+{
+public:
+    Penne() = default;
+    ~Penne() = default;
+
+public:
+    double ileMaki(unsigned P) const override;
+
+};
+
+class Tagliatelle : public Makaron
+{
+public:
+    Tagliatelle();
+    Tagliatelle(double, double, double);
+    ~Tagliatelle();
+
+public:
+    double ileMaki(unsigned P) const override;
+
+private:
+    double L;
+    double W;
+    double R;
+
+    static const double C;
+
+};
+
+Tagliatelle::Tagliatelle()
+{
+    L = 0.5;
+    W = 0.5;
+    R = 0.5;
+}
+
+Tagliatelle::Tagliatelle(double a, double b, double c) :L(a), W(b), R(c)
+{
+}
+
+Tagliatelle::~Tagliatelle()
+{
+}
+
+double Tagliatelle::ileMaki(unsigned P) const
+{
+    return P * L * W *(1. - R) * C;
+}
+const double Tagliatelle::C = 0;
+int main()
+{
+
+}
+
+double Penne::ileMaki(unsigned P) const
+{
+    return P;
+}
 // tutaj definicja metody gotujMakaron
+Makaron* Makaron::gotujMakaron(std::string& str)
+{
+    if (str.front() == str.back()) {
+        Tagliatelle * obj = new Tagliatelle(3.14, 0.42, 0.1);
+        return obj;
+    }
+    else {
+        Penne* obj = new Penne();
+        return obj;
+    }
+}
