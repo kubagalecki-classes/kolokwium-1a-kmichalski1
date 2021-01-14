eda0a085f971b3c7ceb66a42265f39736d62a138
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2021-01-14 13:51:37 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -18,36 +18,26 @@ public:
 class Tagliatelle : public Makaron
 {
 public:
-    Tagliatelle();
-    Tagliatelle(double, double, double);
-    ~Tagliatelle();
+    Tagliatelle::Tagliatelle() :L{0.5} W{0.5}, R{0.5}
+    {
 
-public:
-    double ileMaki(unsigned P) const override;
+    }
+
+    Tagliatelle::Tagliatelle(double a, double b, double c) :L{a}, W{b}, R{c}
+    {
+    }
+    
+    double ileMaki(unsigned P) const override
+    {
+        return P * L * W * (1. - R) * C;
+    }
 
 private:
     double L;
     double W;
     double R;
-
+    
     static const double C;
 
 };
 
-Tagliatelle::Tagliatelle() :L(0.5), W(0.5), R(0.5)
-{
-
-}
-
-Tagliatelle::Tagliatelle(double a, double b, double c) :L(a), W(b), R(c)
-{
-}
-
-Tagliatelle::~Tagliatelle()
-{
-}
-
-double Tagliatelle::ileMaki(unsigned P) const
-{
-    return P * L * W * (1. - R) * C;
-}
