28361f695ce8e49d037c5d52ed171a49f261a8ec
kmichalski1 <73621723+kmichalski1@users.noreply.github.com>
2021-01-14 12:24:31 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,44 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+
+#include <iostream>
+
+class Tagliatelle
+{
+public:
+    Tagliatelle();
+    Tagliatelle(double, double, double);
+    ~Tagliatelle();
+
+public:
+    double ileMaki(unsigned P) const;
+
+private:
+    double L;
+    double W;
+    double R;
+
+    static const double C;
+
+};
+
+Tagliatelle::Tagliatelle()
+{
+    L = 0.5;
+    W = 0.5;
+    R = 0.5;
+}
+
+Tagliatelle::Tagliatelle(double a, double b, double c) :L(a), W(b), R(c)
+{
+}
+
+Tagliatelle::~Tagliatelle()
+{
+}
+
+double Tagliatelle::ileMaki(unsigned P) const
+{
+    return P * L * W *(1. - R) * C;
+}
